fae12d0386e94aaeca3bb7f272910cbe02e616a1
Rampelli Charan Kumar <rampellicharankumar02@gmail.com>
2023-03-10 17:23:16 +0530

    First commit

---
+++ React practice/react-demo/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App" class="container">
+      <h1>Hello World!</h1>
+      <h2>Hi this is charan</h2>
+      <h2>Welcome</h2>
+    </div>
+  );
+}
+
+export default App;
